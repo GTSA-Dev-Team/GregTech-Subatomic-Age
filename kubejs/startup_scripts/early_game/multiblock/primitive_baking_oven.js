9d694aab3a8edbc46f91495601a7c695b41d1e7f
tolek tpr <tolek@epsi.pl>
2025-12-27 10:54:51 +0100

    Organization and primitive machines

--- kubejs/startup_scripts/early_game/multiblock/primitive_baking_oven.js
+++ kubejs/startup_scripts/early_game/multiblock/primitive_baking_oven.js
@@ -1,14 +1,7 @@
-GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
-    event.create('primitive_baking_oven')
-        .category('primitive')
-        .setMaxIOSize(2, 1, 0, 0)
-        .setSound(GTSoundEntries.FURNACE)
-});
-
 GTCEuStartupEvents.registry('gtceu:machine', event => {
     event.create("gtsac:primitive_baking_oven", "primitive")
         .rotationState(RotationState.ALL)
-        .recipeType('primitive_baking_oven')
+        .recipeType('smoker')
         .pattern(definition => FactoryBlockPattern.start()
             .aisle('CCC', 'CCC', 'CCC')
             .aisle('CCC', 'CAC', 'CAC')
