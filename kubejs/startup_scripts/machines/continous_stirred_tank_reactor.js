5a2f626d91fb4fa6f977dc6551b6f7f80150ca9a
tolek tpr <tolek@epsi.pl>
2025-12-30 22:58:38 +0100

    added main catalyst material, just need to replace polyethylene prod

---
+++ kubejs/startup_scripts/machines/continous_stirred_tank_reactor.js
@@ -0,0 +1,15 @@
+GTCEuStartupEvents.registry("gtceu:recipe_type", event => {
+    event.create('cstr')
+        .category('basic')
+        .setMaxIOSize(3, 3, 3, 3)
+        .setProgressBar(GuiTextures.PROGRESS_BAR_BATH, FillDirection.LEFT_TO_RIGHT)
+        .setSound(GTSoundEntries.BATH)
+})
+
+GTCEuStartupEvents.registry('gtceu:machine', event => {
+    event.create('cstr', 'simple')
+    .definition((tier, builder) => {
+        builder.recipeType('cstr')
+        .workableCasingModel(`gtceu:block/casings/voltage/${tierToHullMap[tier]}/side`, 'gtceu:block/machines/chemical_bath')
+    })
+})
