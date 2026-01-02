9b46ad372f28f0f1e326f7bd6e5c09c467cae660
tolek tpr <tolek@epsi.pl>
2026-01-02 20:57:47 +0100

    Started LV quest line

--- kubejs/server_scripts/recipes/battery_recipes.js
+++ kubejs/server_scripts/recipes/battery_recipes.js
@@ -13,7 +13,7 @@ ServerEvents.recipes(event => {
         '   '
     ], { C: "gtceu:tin_single_cable", P: "gtceu:battery_alloy_plate" })
 
-    event.shaped('gtsac:lead_batter_anode', [
+    event.shaped('gtsac:lead_battery_anode', [
         '   ',
         ' PC',
         '   '
