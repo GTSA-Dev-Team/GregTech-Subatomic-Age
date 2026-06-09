fcc01fc49d93d06d0e367277ac0219a15a4d8812
Tolek <tolek@epsi.pl>
2026-06-09 19:51:29 +0200

    Test

---
+++ kubejs/ts/src/server_scripts/test.ts
@@ -0,0 +1,5 @@
+import { RecipesEventJS } from "@pts/dev/latvian/mods/kubejs/recipe";
+import { RecipeFilter } from "@pts/dev/latvian/mods/kubejs/recipe/filter";
+
+RecipesEventJS.prototype.remove(RecipeFilter.of("minecraft:stick"))
+
