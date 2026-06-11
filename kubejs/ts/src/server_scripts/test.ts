045a61c14c0c78584da7389bc3228df7891309fb
Tolek <tolek@epsi.pl>
2026-06-11 18:02:52 +0200

    tried some more, failed some more

--- kubejs/ts/src/server_scripts/test.ts
+++
@@ -1,7 +0,0 @@
-import { RecipesEventJS } from "@pts/dev/latvian/mods/kubejs/recipe";
-import { RecipeFilter } from "@pts/dev/latvian/mods/kubejs/recipe/filter";
-
-ServerEvents.blockLootTables(gt => {
-
-});
-
