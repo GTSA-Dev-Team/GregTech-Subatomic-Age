13358a06b9273951d1ebaeb75401542478d488e6
Tolek <tolek@epsi.pl>
2026-06-14 13:08:54 +0200

    added durations and eu for silicon line

--- kubejs/startup_scripts/gtceu/item/mv/items.js
+++ kubejs/startup_scripts/gtceu/item/mv/items.js
@@ -1,7 +1,10 @@
 StartupEvents.registry('item', event => {
-    event.create('gtceu:silicon_rods').texture('gtceu:item/wafers/silicon_rods')
     event.create('gtceu:silicon_chunks').texture('gtceu:item/wafers/silicon_chunks')
     event.create('gtceu:silicon_seed_crystal').texture('gtceu:item/wafers/silicon_seed_crystal')
     event.create('gtceu:quartz_crucible').texture('gtceu:item/wafers/quartz_crucible')
     event.create('gtceu:raw_silicon_wafer').texture('gtceu:item/wafers/raw_silicon_wafer')
+})
+
+GTCEuStartupEvents.registry('gtceu:material', (event) => {
+    GTMaterials.Silicon.addFlags(GTMaterialFlags.GENERATE_ROD)
 })
